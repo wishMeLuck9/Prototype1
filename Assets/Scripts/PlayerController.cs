b44fc48a81d2cdd545287cd541283c1e6779ddfb
Lik099 <149300259+wishMeLuck9@users.noreply.github.com>
2026-02-27 10:03:06 +0000

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
      // Move the vehicle forward
-     transform.Translate(0,0,1);
+     transform.Translate(Vector3.forward);
     }
 }
