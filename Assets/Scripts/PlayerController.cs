291edf370b923e9af7ac15edbabe6a60f7bf51a2
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-03-04 19:25:32 +0000

    lr

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float speed = 25.0f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,8 +15,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     // Move the vehicle forward
-     transform.Translate(Vector3.forward * Time.deltaTime * speed);
-     transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+     transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
