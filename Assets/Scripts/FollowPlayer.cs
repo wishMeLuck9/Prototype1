eb9475cfa736907e4a55a9147f8180cc774110ab
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-03-04 15:34:20 +0000

    Update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player; // Reference to the player GameObject
+    private Vector3 offset = new Vector3(0, 5, -7); // Offset from the player
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7); // Set the position of this GameObject to the player's position
+        transform.position = player.transform.position + offset; // Set the position of this GameObject to the player's position
     }
 }
